f102c15191fcee4355014c7b7bd29c82c8244733
Coby Lovelace <jtl5jjn@virginia.edu>
2023-03-06 18:16:48 -0500

    included chord board in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ChordBoard from './components/ChordBoard';
 
 function App() {
   return (
     <div className="App">
       Hello World!
+      <ChordBoard />
     </div>
   );
 }
