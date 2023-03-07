368c36a8fb4a4ddfd36149f83036e44f1072551a
Coby Lovelace <jtl5jjn@virginia.edu>
2023-03-06 22:46:56 -0500

    UI touchups on navbar

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -3,7 +3,23 @@ import "./TopBar.css";
 function TopBar() {
   return (
     <div className="top-bar-container">
-      top bar
+
+        <div className="upper">
+          <div className="content">
+            <h1>CHORDBOARD</h1>
+            <div className="account">
+              <button className="sign-up">Sign Up</button>
+              <button className="log-in">Log In</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="lower">
+          <div className="content">
+            nav links
+          </div>
+        </div>  
+
     </div>
   );
 }
